acea8f3cee98a606ce50e3565fad29f8861ff83a
Mikael Hellman <mikael.hellman@gmail.com>
2020-03-28 15:48:52 +0100

    Cache

--- routes.js
+++ routes.js
@@ -1,12 +1,16 @@
 const DataProcessor = require('./lib/data-processor')
 
+let data = null
+
 module.exports = {
   attachRoutes: (server) => {
 
     server.get('/data.json', async (req, res) => {
       try {
-        const data = await DataProcessor.getData()
-        res.json(formatFrontend(data))
+        if (!data) {
+          data = await DataProcessor.getData()
+        }
+        res.json(data)
       } catch (e) {
         console.log(e)
         res.status(500).send()
@@ -15,6 +19,6 @@ module.exports = {
   }
 }
 
-function formatFrontend(data) {
-  return data
-}
+setInterval(() => {
+  data = null
+}, 3600000)
